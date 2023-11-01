e7fc5309079771d5bf628854bb3e9726a1fe2aa2
Shinu-ster <sajatbazz@gmail.com>
2023-11-01 16:21:55 +0545

    added display

--- pages/display.php
+++ pages/display.php
@@ -1,13 +1,5 @@
 <?php
     include '../database/dbconnect.php';
-    session_start();
-    $profile = $_SESSION['id'];
-    if($profile == true){
-        //allow to use this page only if session exists
-        // echo "session exists";
-    }else{
-        header('location:http://localhost/4thsemProj/login/login.php');
-    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,8 +7,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../styles/display.css">
 </head>
 <body>
-<embed src="../pdf/Untitled 1.pdf" type="application/pdf" width="1000px" height="600px" />
+    <?php
+    $sql = "SELECT * FROM pdf";
+    $result = mysqli_query($conn,$sql);
+    $num = mysqli_num_rows($result);
+    if($num>0){
+       while( $row = mysqli_fetch_assoc($result)){;
+    
+    ?>
+    <div class="wrapper">
+        <div class="image">
+    <img src="<?php echo $row['cover']?>"alt="" srcset="" height="100px"><br><br>
+     <p><?php
+           echo $row['name'];
+        ?>
+        </p>
+    </div>
+       
+        <br>
+        <div class="description">
+        <?php
+            echo $row['description'];
+        ?></div>
+        <br>
+        
+    </div>
+    <?php
+    }
+}
+    ?>
 </body>
 </html>
