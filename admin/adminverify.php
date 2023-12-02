f7e4dbfc16a03e4dd4a2e5a0a2f6d7c7bd2eb152
shinu-ster <sajatbazz@gmail.com>
2023-12-02 22:11:20 +0545

    Added Admin Page

---
+++ admin/adminverify.php
@@ -0,0 +1,23 @@
+<?php
+
+    include '../database/dbconnect.php';
+    session_start();
+   if( $_SESSION['is_admin'] = 5){
+    echo $_SESSION['is_admin'];
+   }
+   else{
+    header('location:http://localhost/4thsemProj/crud/addpdf.php');
+   }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
