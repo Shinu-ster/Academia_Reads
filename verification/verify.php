b6fdc672b8bf12720c3123e146a04b6e15475a26
shinu-ster <sajatbazz@gmail.com>
2023-12-02 22:02:59 +0545

    Fixed verify bug

--- verification/verify.php
+++ verification/verify.php
@@ -23,21 +23,74 @@ if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
 <body>
     <?php
     include_once '../components/navbar.php';
-    $sql = "SELECT COUNT(is_verify) AS count
-    FROM pdf
-    WHERE is_verify = 0 AND id = $profile";
-    $res = mysqli_query($conn, $sql);
+    // $sql = "SELECT COUNT(is_verify) AS count
+    // FROM pdf
+    // WHERE is_verify = 0 AND id = $profile";
+    // $res = mysqli_query($conn, $sql);
 
-    if ($res) {
-        $row = mysqli_fetch_assoc($res);
-        // echo $profile;
-        $count = $row['count'];
+    // if ($res) {
+    //     $row = mysqli_fetch_assoc($res);
+    //     // echo $profile;
+    //     $count = $row['count'];
 
-        echo "Count <sup>" . $count. "</sup>";
-    } else {
-        echo "Error: " . mysqli_error($conn);
-    }
+    //     echo "Count <sup>" . $count. "</sup>";
+    // } else {
+    //     echo "Error: " . mysqli_error($conn);
+    // }
 // echo "Profile" .$profile;
+// $sql = "SELECT * FROM pdf where id = profile ";
+// $result = mysqli_query($conn, $sql);
+// $num = mysqli_num_rows($result);
+// if ($num > 0) {
+//     while ($row = mysqli_fetch_assoc($result)) {
+// // echo $num;
+// // echo $profile;
+// // echo $count;
+    
+
+    ?>
+   
+    <table border=1>
+        <tr>
+       
+            <th>Name</th>
+            <th>Description</th>
+            <th>Cover</th>
+            <th>Status</th>
+        </tr>
+       <tr>
+            <td>
+            <?php
+        // echo "Profile" .$profile;
+        $sql = "SELECT * FROM pdf where id = $profile AND is_verify = 0";
+        $result = mysqli_query($conn, $sql);
+        $num = mysqli_num_rows($result);
+        if ($num > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+    ?>
+    <?php
+        echo $row['name'];
+    ?>
+            </td>
+            <td><?php
+            echo $row['description']
+            ?></td>
+            <td>
+                <img src=" <?php
+                    echo $row['cover']
+                ?>" alt="" srcset="" height="20px" width="20px">
+               
+            </td>
+            <td>
+                <?php
+                    echo $row['is_verify']
+                ?>
+            </td>
+       </tr>    
+    </table>
+    <?php
+    }
+}
     ?>
 </body>
 
