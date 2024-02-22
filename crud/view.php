801a6175f8b82ad9327cf8221e5bb91614c2b6a3
shinu-ster <sajatbazz@gmail.com>
2024-02-22 11:22:12 +0545

    Improved Page layout

--- crud/view.php
+++ crud/view.php
@@ -1,34 +1,60 @@
 <?php
-    include '../database/dbconnect.php';
-    session_start();
-    $profile = $_SESSION['id'];
-    $pdfid = $_GET['view'];
-    if($profile == true){
-        //allow to use this page only if session exists
-        // echo "session exists";
-    }else{
-        header('location:http://localhost/4thsemProj/login/login.php');
-    }
-    
-    $sql = "SELECT * FROM pdf where f_id = $pdfid";
-    $result = mysqli_query($conn,$sql);
-    $row = mysqli_fetch_assoc($result);
+include '../database/dbconnect.php';
+session_start();
+$profile = $_SESSION['id'];
+$pdfid = $_GET['view'];
+if ($profile == true) {
+    //allow to use this page only if session exists
+    // echo "session exists";
+} else {
+    header('location:http://localhost/4thsemProj/login/login.php');
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/global.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../styles/view.css?v=<?php echo time(); ?>">
+
     <title>Document</title>
-</head>
-<body>
     <?php
-    echo $row['name'];
-    
+    include_once '../components/navbar.php';
     ?>
-<embed src="<?php
-    echo $row['file'];
-?>" type="application/pdf" width="1260px" height="600px" />
+</head>
+
+<body>
+    <div class="container">
+        <?php
+        $sql = "SELECT * FROM pdf where f_id = $pdfid";
+        echo $pdfid;
+        $result = mysqli_query($conn, $sql);
+        $num = mysqli_num_rows($result);
+        echo $num;
+        if ($num == 1) {
+            while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+                <table border="1">
+                    <p>hello</p>
+                    <tr>
+                        <td>
+
+                            <?php
+                            echo $row['name'];
+                            ?>
+                        </td>
+                        <td>hello//</td>
+                    </tr>
+                </table>
+    </div>
+<?php
+
+            }
+        } ?>
+<p>htmlspecialchars_decode</p>
 </body>
+
 </html>
